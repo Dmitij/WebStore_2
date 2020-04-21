27f10ddb593c9408c5a3c1d2f0eafbb125f1c9f6
Дмитрий Р <gorbunok_@mail.ru>
2020-04-21 22:06:12 +0300

    Подключение сервисов в проект WebAPI

--- Services/WebStoreServiceHosting/Startup.cs
+++ Services/WebStoreServiceHosting/Startup.cs
@@ -4,11 +4,18 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using WebStore.DAL.Context;
+using WebStore.Infrastructure.Interfaces;
+using WebStore.Infrastructure.Services.InCookies;
+using WebStore.Infrastructure.Services.InMemory;
+using WebStore.Infrastructure.Services.InSQL;
 
 namespace WebStoreServiceHosting
 {
@@ -25,6 +32,27 @@ namespace WebStoreServiceHosting
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            #region Сервисы бизнес-логики
+
+            services.AddSingleton<IEmployeesData, InMemoryEmployeesData>();
+            services.AddScoped<IProductData, SqlProductData>();
+            services.AddScoped<IOrderService, SqlOrderService>();
+            services.AddScoped<ICartService, CookiesCartService>();
+
+            #endregion
+
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();  //нужен чтобы CookiesCartService смог создаться
+
+
+            #region База данных
+
+            services.AddDbContext<WebStoreDB>(opt =>
+                   opt.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            //services.AddTransient<WebStoreDBInitializer>();
+
+            #endregion
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
